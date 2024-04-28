b0b5fb5826a0675d300c193b57f271e4938e52fc
gukazma <gukazma@proton.me>
2024-04-28 13:24:16 +0800

    ✨ feat: 参数化mesh

--- Apps/DemoApp/main.cpp
+++ Apps/DemoApp/main.cpp
@@ -1,33 +1,46 @@
 #include <rerun.hpp>
 #include <vector>
 #include <CGAL/Polygon_mesh_processing/IO/polygon_mesh_io.h>
+#include <CGAL/Surface_mesh_parameterization/IO/File_off.h>
+#include <CGAL/Surface_mesh_parameterization/parameterize.h>
+#include <CGAL/Polygon_mesh_processing/measure.h>
 #include <CGAL/Simple_cartesian.h>
 #include <CGAL/Surface_mesh.h>
 #include <CGAL/bounding_box.h>
 
-typedef CGAL::Simple_cartesian<double> K;
-typedef K::Point_3 Point;
-typedef CGAL::Surface_mesh<K::Point_3> Mesh;
-typedef Mesh::Vertex_index             vertex_descriptor;
-typedef boost::graph_traits<Mesh>::face_descriptor   face_descriptor;
-typedef boost::graph_traits<Mesh>::halfedge_descriptor halfedge_descriptor;
-typedef boost::graph_traits<Mesh>::vertex_descriptor vertex_descriptor;
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2                                Point_2;
+typedef Kernel::Point_3                                Point_3;
+typedef CGAL::Surface_mesh<Kernel::Point_3>            SurfaceMesh;
+typedef boost::graph_traits<SurfaceMesh>::vertex_descriptor   vertex_descriptor;
+typedef boost::graph_traits<SurfaceMesh>::halfedge_descriptor halfedge_descriptor;
+typedef boost::graph_traits<SurfaceMesh>::face_descriptor     face_descriptor;
 
 namespace PMP = CGAL::Polygon_mesh_processing;
+namespace SMP = CGAL::Surface_mesh_parameterization;
+
  int main() {
 
-    Mesh m;
+    SurfaceMesh sm;
 
-    if (!PMP::IO::read_polygon_mesh("D:/data/ply/bunny.ply", m)) {
+    if (!PMP::IO::read_polygon_mesh("D:/data/ply/bunny.ply", sm)) {
         std::cerr << "Error: Invalid input." << std::endl;
         return EXIT_FAILURE;
     }
+    halfedge_descriptor bhd    = CGAL::Polygon_mesh_processing::longest_border(sm).first;
+    typedef SurfaceMesh::Property_map<vertex_descriptor, Point_2> UV_pmap;
+    UV_pmap uv_map = sm.add_property_map<vertex_descriptor, Point_2>("h:uv").first;
 
-    auto bbox = CGAL::bounding_box(m.points().begin(), m.points().end());
+    SMP::parameterize(sm, bhd, uv_map);
+    std::ofstream out("D:/result.off");
+    SMP::IO::output_uvmap_to_off(sm, bhd, uv_map, out);
+
+
+    auto bbox = CGAL::bounding_box(sm.points().begin(), sm.points().end());
     auto max  = bbox.max();
     auto min  = bbox.min();
     auto center = (max - min)/2;
-    for (auto& p : m.points()) {
+    for (auto& p : sm.points()) {
         p -= center;
     }
     //// Add the points as vertices
@@ -49,15 +62,16 @@ namespace PMP = CGAL::Polygon_mesh_processing;
      std::vector<rerun::Color>      vertex_colors;
      std::vector<uint32_t>    indices;
 
-     for (vertex_descriptor v : CGAL::vertices(m)) {
-        rerun::Position3D p = {(float)m.point(v).x(), (float)m.point(v).y(), (float)m.point(v).z()};
+     for (vertex_descriptor v : CGAL::vertices(sm)) {
+        rerun::Position3D p = {
+            (float)sm.point(v).x(), (float)sm.point(v).y(), (float)sm.point(v).z()};
         vertices.push_back(p);
         vertex_colors.push_back({255, 255, 0});
      }
 
-     for (face_descriptor f : CGAL::faces(m)) {
-        CGAL::Vertex_around_face_iterator<Mesh> vbegin, vend;
-        for (boost::tie(vbegin, vend) = CGAL::vertices_around_face(CGAL::halfedge(f, m), m);
+     for (face_descriptor f : CGAL::faces(sm)) {
+        CGAL::Vertex_around_face_iterator<SurfaceMesh> vbegin, vend;
+        for (boost::tie(vbegin, vend) = CGAL::vertices_around_face(CGAL::halfedge(f, sm), sm);
              vbegin != vend;
              ++vbegin) {
             indices.push_back(*vbegin);
@@ -75,12 +89,12 @@ namespace PMP = CGAL::Polygon_mesh_processing;
      std::vector<rerun::Color>      colors;
 
 
-     for (halfedge_descriptor hf : CGAL::halfedges(m)) {
-        vertex_descriptor source = CGAL::source(hf, m);
-        vertex_descriptor target = CGAL::target(hf, m);
+     for (halfedge_descriptor hf : CGAL::halfedges(sm)) {
+        vertex_descriptor source = CGAL::source(hf, sm);
+        vertex_descriptor target = CGAL::target(hf, sm);
 
-        auto sourcePoint = m.point(source);
-        auto targetPoint = m.point(target);
+        auto sourcePoint = sm.point(source);
+        auto targetPoint = sm.point(target);
         auto v = targetPoint - sourcePoint;
         origins.push_back({(float)sourcePoint.x(), (float)sourcePoint.y(), (float)sourcePoint.z()});
         vectors.push_back({(float)v.x(), (float)v.y(), (float)v.z()});
