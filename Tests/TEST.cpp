09f56d5a93975508abf54510c2aa560472d309d6
gukazma <gukazma@proton.me>
2024-12-05 15:30:30 +0800

    ✨ feat: 更新vcpkg

--- Tests/TEST.cpp
+++ Tests/TEST.cpp
@@ -1,5 +1,14 @@
 #include <gtest/gtest.h>
+#include <CGAL/Linear_cell_complex_for_combinatorial_map.h>
+#include <CGAL/draw_linear_cell_complex.h>
 TEST(MYTEST0, A)
 {
-
+	using LCC = CGAL::Linear_cell_complex_for_combinatorial_map<3>;
+	using Point = LCC::Point;
+	LCC lcc;
+	lcc.make_hexahedron(Point(0, 0, 0), Point(5, 0, 0),
+		Point(5, 5, 0), Point(0, 5, 0),
+		Point(0, 5, 4), Point(0, 0, 4),
+		Point(5, 0, 4), Point(5, 5, 4));
+	CGAL::draw(lcc);
 }
